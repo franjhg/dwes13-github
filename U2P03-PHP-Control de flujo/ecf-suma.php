14a0d0a18dc97ebba143ff0ad9f4db421c7b5c30
crish <crish@192.168.1.128>
2017-10-24 19:59:00 +0200

    24/10/17/02

---
+++ U2P03-PHP-Control de flujo/ecf-suma.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8/">
+</head>
+<body>
+<?php
+    if(!isset($_POST['Enviar'])){
+?>
+        <form action="ecf-suma.php" method="post">
+        	Numero: <input type="number" name="Numero">
+        	<input type="submit" name="Enviar">
+        </form>
+<?php
+    }else {
+       $n=$_POST["Numero"];
+       $acum=0;
+	   for($i=1;$i<=$n;$i++){
+	       $acum+=$i;
+	   }
+	   echo $acum;
+    }
+
+?>
+  	<a href="index.php">VOLVER</a>  
+</body>
+</html>
+<?php
