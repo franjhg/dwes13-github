31129a1889f6c17fa3f3ea38eada4862de549fc2
crish <crish@192.168.1.128>
2017-10-25 19:10:50 +0200

    25/10/17casa

--- U2P03-PHP-Control de flujo/ecf-meses.php
+++ U2P03-PHP-Control de flujo/ecf-meses.php
@@ -8,13 +8,43 @@
 ?>
         <form action="ecf-meses.php" method="post">
         	Mes: <input type="text" name="mes">
-        	Bisiesto: <input type="checkbox" value="bisiesto">
+        	<!-- Bisiesto: <input type="checkbox" value="bisiesto"> -->
         	<input type="submit" name="Enviar">
         </form>
 <?php
     }else {
         $mes=$_POST["mes"];
-        $bis=$_POST
+       /*  $bis=$_POST["bisiesto"]; */
+        $arraymes31=array("1","enero","3","marzo","5","mayo","7"."julio","8",
+            "agosto","10","octubre","12","diciembre");
+        $arraymes30=array("4","abril","6","junio","9","septiembre","11","noviembre");
+        $sw31=false;
+        $sw30=false;
+        
+        
+       /*  if($bis=="bisiesto" && $mes==2){
+            echo "El mes ".$mes." tiene 29 dias";
+        }
+        if($bis!="bisiesto" && $mes==2){
+            echo "El mes ".$mes." tiene 28dias";
+        }  */
+        for($i=0;$i<=sizeof($arraymes31)-1;$i++){
+            if(strcasecmp($mes, $arraymes31[$i])==0){
+                $sw31=true;
+            }
+        }
+        for($i=0;$i<=sizeof($arraymes30)-1;$i++){
+            if(strcasecmp($mes, $arraymes30[$i])==0){
+                $sw30=true;
+            }
+        }
+        
+        if($sw31==true){
+            echo "El mes ".$mes." tiene 31 dias";
+        }
+        if($sw30==true){
+            echo "El mes ".$mes." tiene 30 dias";
+        }
         
     }
 ?>
