25f181befafeac4ce6a0d57b2694a92e33aafb4c
Alumno <Alumno@W7Bsica>
2017-11-13 11:19:15 +0100

    13/11/17

--- U2P05_3-PHP-Validaciones/validaciones.php
+++ U2P05_3-PHP-Validaciones/validaciones.php
@@ -59,18 +59,53 @@
         return $errorFecha;
     }
     
-    function validarContraseña($password){
-        if(!empty($password)){
-            if($password>6 && $password<16){
-                $errorPass=false;
-                }else{
-                $errorPass=true;
-                }
-        }else{
-        $errorPass=true;
+    function validarContraseña($clave){
+        
+            if(strlen($clave) < 6){
+                
+                $error_clave = "La clave debe tener al menos 6 caracteres";
+                
+                return false;
+                
+            }
+            
+            if(strlen($clave) > 16){
+                
+                $error_clave = "La clave no puede tener más de 16 caracteres";
+                
+                return false;
+                
+            }
+            
+            if (!preg_match('`[a-z]`',$clave)){
+                
+                $error_clave = "La clave debe tener al menos una letra minúscula";
+                
+                return false;
+                
+            }
+            
+            if (!preg_match('`[A-Z]`',$clave)){
+                
+                $error_clave = "La clave debe tener al menos una letra mayúscula";
+                
+                return false;
+                
+            }
+            
+            if (!preg_match('`[0-9]`',$clave)){
+                
+                $error_clave = "La clave debe tener al menos un caracter numérico";
+                
+                return false;
+                
+            }
+            
+            
+            
+            return true;
+            
         }
-        return $errorPass;
-    }
 
     function validarNombre($nom){
         if(!empty($nom)){
