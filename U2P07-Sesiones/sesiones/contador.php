a01c6c94877e6686fa379dcf53554f4659eaa3d5
franjhg <32298853+franjhg@users.noreply.github.com>
2017-11-21 09:25:01 +0100

    Add files via upload
    21/11_2

--- U2P07-Sesiones/sesiones/contador.php
+++ U2P07-Sesiones/sesiones/contador.php
@@ -1,11 +1,19 @@
 <?php
-if (session_status () == PHP_SESSION_NONE)
+
+//U2P07_2.4
+
+if (session_status () == PHP_SESSION_NONE){
+    session_name('idSesion13');
     session_start ();
+    if (isset($_REQUEST["reiniciarContador"])) {
+        unset($_SESSION["contador"]);
+    }
     if (isset ( $_SESSION ['contador'] ))
         $_SESSION ['contador'] += 1;
         else
             $_SESSION ['contador'] = 1;
             $mensaje = "Has visitado esta página " . $_SESSION ['contador'] . " veces en esta sesión.";
+}
 ?>
 <html>
 <head>
@@ -15,5 +23,8 @@ if (session_status () == PHP_SESSION_NONE)
 <body>
 <h3><?php echo $mensaje;?></h3>
 <p><a href="<?php echo $_SERVER['PHP_SELF']?>">Recargar la página</a></p>
+
+	<p><a href="<?php echo $_SERVER['PHP_SELF']."?reiniciarContador=true"?>">Reiniciar contador</a></p>
+
 </body>
 </html>
---
+++ U2P07-Sesiones/sesiones/contador_2.php
@@ -0,0 +1,53 @@
+<?php
+if(session_status() == PHP_SESSION_NONE) {
+    // código de renombrado de la sesión
+    session_start();
+}
+if(isset($_POST['enviar'])){
+    $_SESSION['contador']+=$_POST['visitas'];
+}
+if (isset($_REQUEST["reiniciarContador"])) {
+    unset($_SESSION["contador"]);
+}
+if (isset($_REQUEST["cerrarSesion"])) {
+    $_SESSION=array();
+    session_unset();
+    if (ini_get("session.use_cookies")) {
+        $params = session_get_cookie_params();
+        setcookie(session_name(), '', time() - 42000,
+            $params["path"], $params["domain"],
+            $params["secure"], $params["httponly"]
+            );
+    }
+    session_destroy();
+}
+
+if(session_status() == PHP_SESSION_NONE)
+    $mensaje = "No hay sesión iniciada";
+    else {
+        if(isset( $_SESSION['contador']))
+            $_SESSION['contador']+= 1;
+            else
+                $_SESSION['contador']=1;
+                $mensaje = "Has visitado esta página ". $_SESSION['contador']." veces en esta sesión.";
+    }
+    ?>
+<html>
+<head>
+<title>Sesiones</title>
+<meta charset="UTF-8"/>
+</head>
+<body>
+<h3><?php echo $mensaje;?></h3>
+<p><a href="<?php echo $_SERVER['PHP_SELF']?>">Recargar la página</a></p>
+<p><a href="<?php echo $_SERVER['PHP_SELF']."?reiniciarContador=true"?>">Reiniciar contador</a></p>
+<p><a href="<?php echo $_SERVER['PHP_SELF']."?cerrarSesion=true"?>">Cerrar sesión</a></p>
+
+<div>
+	<form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+		Modificar visitas:<input type="number" name="visitas">
+		<input type="submit" name="enviar">
+	</form>
+
+</div>
+</body></html>
