d53412ef6a332d320a36f7e0a7507ea17ef017c4
franjhg <franjhg@github.com>
2017-12-04 11:17:23 +0100

    04/12/17

---
+++ U2P08-PHP-Sesiones2/login/index.php
@@ -0,0 +1,14 @@
+<?php 
+
+
+
+?>
+<html>
+	<head>
+		<title>Sesiones</title>
+    	<meta charset="UTF-8"/>
+	</head>
+	<body>
+    	<h3>Hola<?php echo $mensaje;?></h3>
+	</body>
+</html>
---
+++ U2P08-PHP-Sesiones2/login/login.php
@@ -0,0 +1,20 @@
+<?php
+include('conexion.php');
+   
+   if(session_status()==PHP_SESSION_NONE){
+       session_name('idSesion13');
+       session_start ();
+   }
+   
+   if(isset($_POST["enviar"])){
+       $nombre=$_REQUEST["nombre"];
+       $passw=$_REQUEST["passwd"];
+   }
+?>
+	<div>
+  		<form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+		Nombre:<input type="text" name="nombre">
+		Contraseña:<input type="text" name="passwd">
+		<input type="submit" name="enviar">
+	</form>
+  	</div>     
