419c4d415f41c984670dc812d5180ec71c64b95f
franjhg <franjhg@github.com>
2018-01-24 12:36:13 +0100

    222

---
+++ U4P06-Redireccion y reenvio/src/servlets/GenerarNumeroServlet.java
@@ -0,0 +1,50 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class GenerarNumeroServlet
+ */
+@WebServlet("/Sorpresa")
+public class GenerarNumeroServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GenerarNumeroServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		int res=(int) (Math.random()*100+1);
+		PrintWriter out = response.getWriter();
+		out.println("<html><head><meta charset='UTF-8'/><title>Numero Aleatorio</title></head>");
+		out.println("<body><h1>"+res+"</h1>");
+		out.println("</body></html>");
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
