ce205b7d319418ea38c054300cc310a0a779b7dd
crish <crish@192.168.1.131>
2018-01-29 06:55:33 +0100

    29/01

---
+++ U4P07-Java-DatosPersonales/src/servlets/ProcesarDatosServlet.java
@@ -0,0 +1,57 @@
+package servlets;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ProcesarDatosServlet
+ */
+@WebServlet("/ProcesarDatos")
+public class ProcesarDatosServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ProcesarDatosServlet() {
+        super();
+        
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+										
+		//text							--campo name--
+		String nombre=request.getParameter("nombre");
+		String apellidos=request.getParameter("apellidos");
+		
+		//date
+		String fnacimiento=request.getParameter("fnacimiento");
+		/*SimpleDateFormat formatoFecha=new SimpleDateFormat("yyyy-MM-dd");
+		try {
+		Date fnacimiento=formatoFecha.parse(request.getParameter("fnacimiento"));
+			SimpleDateFormat formatoFechaSalida=new SimpleDateFormat("dd/MM/yyyy");
+		}*/
+		
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
