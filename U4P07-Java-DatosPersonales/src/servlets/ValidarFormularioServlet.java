1b5fb0522ddc84f5cb5d19f4d0f01be03ef46053
franjhg <franjhg@github.com>
2018-01-31 12:29:19 +0100

    31/01_01

--- U4P07-Java-DatosPersonales/src/servlets/ValidarFormularioServlet.java
+++ U4P07-Java-DatosPersonales/src/servlets/ValidarFormularioServlet.java
@@ -2,12 +2,16 @@ package servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.ParseException;
 
 /**
  * Servlet implementation class ValidarFormularioServlet
@@ -28,29 +32,65 @@ public class ValidarFormularioServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-	//text							--campo name--
-		String nombre=request.getParameter("nombre");
-		String apellidos=request.getParameter("apellidos");	
-		String contraseña=request.getParameter("contraseña");
-		
-		
-		
-		
-		
+		 response.setContentType("text/html;charset=UTF-8");
 		PrintWriter out = response.getWriter();
-		
-		
 		out.println("<html>"
 				+ "<head>"
 				+ "<meta charset='UTF-8'/>"
 				+ "<title>Servlets y Formularios</title>"
+				+"<style type='text/css'>"
+				+ "p{color:red;border:2px solid black;}"
+				+ "</style>"
 				+ "</head>");
 		out.println("<body>"
 				+ "<h1>Servlets y Formularios</h1>");
-	
-	out.println("<p>"+validaContraseña(contraseña)+"</p>");
+	//text							--campo name--
+		String nombre=request.getParameter("nombre");
+		String apellidos=request.getParameter("apellidos");	
+		String password=request.getParameter("password");
+		String telefono=request.getParameter("telefono");
+			String patron="^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&+=]).{3,}$";
+			String patronTelefono="[0-9]*";
+			
+			SimpleDateFormat formatoFecha=new SimpleDateFormat("yyyy-MM-dd");
+			try {
+			Date fecha=formatoFecha.parse(request.getParameter("fnacimiento"));
+			Date fechaActual=new Date();
+			
+				long milisegFecha=fecha.getTime();
+				long milisegFechaActual=fechaActual.getTime();
+				if(milisegFecha<milisegFechaActual) {
+					out.println("Fecha no válida");
+				}else {
+					out.println(fecha);
+				}
+			}catch(ParseException e) {
+				e.printStackTrace();
+			} catch (java.text.ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		
+				
+		
+	
+		out.println("<p>"+nombre+"</p>");
+		
+		if(password==null) {
+			out.println("<p>no hay</p>");
+		}
+		
+		if(password.matches(patron)) {
+			out.println("<p>Contraseña correcta</p>");
+		}else {
+			out.println("<p>Contraseña incorrecta</p>");
+		}
+		
+		if(telefono.matches(patronTelefono)) {
+			out.println("<p>telefono correcto</p>");
+		}else {
+			out.println("<p>telefono incorrecto</p>");
+		}
 		
 		
 		out.println("</body></html>");
@@ -64,10 +104,10 @@ public class ValidarFormularioServlet extends HttpServlet {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
-	public boolean validaContraseña(String contra) {
+	/*public boolean validaContraseña(String contra) {
 		boolean valido=true;
 		
 		
 		return valido;
-	}
+	}*/
 }
