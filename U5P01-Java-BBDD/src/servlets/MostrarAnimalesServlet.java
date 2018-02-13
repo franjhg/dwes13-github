bbaae4e86d5daaa1da16aed574f5732837ebab9d
franjhg <franjhg@github.com>
2018-02-13 14:13:53 +0100

    13/02

---
+++ U5P01-Java-BBDD/src/servlets/MostrarAnimalesServlet.java
@@ -0,0 +1,50 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MostrarAnimalesServlet
+ */
+@WebServlet("/MostrarAnimales")
+public class MostrarAnimalesServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MostrarAnimalesServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ServletContext contexto = getServletContext();
+		response.setContentType("text/html;UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<html><head><meta charset='UTF-8'/></head><body>");
+	
+		
+		out.println("</body></html>");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
